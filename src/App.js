79c2922b566cfea8f727dcfeb2617e2d55352c66
Manikanta M <manikantapoojary444@gmail.com>
2023-03-12 12:34:19 +0530

    added counter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState,useEffect } from 'react'
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    document.title=`Chats(${count})`
+  })
+  
+  const increment=()=>{
+    setCount(count+1);
+  }
+  const decrement=()=>{
+    count>0?setCount(count-1):setCount(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+   <div className="container">
+    <p>{count}</p>
+    <button onClick={()=>increment()}>INCR</button>
+    <button onClick={()=>decrement()}>DECR</button>
+   </div>
+  </>
+  )
 }
 
-export default App;
+export default App
